755e5c589ae63df4db4b3d6f6d07ce4d5f8c7ca8
WANDOKING <dowankim57@gmail.com>
2023-02-20 20:43:53 +0900

    version 1.0.0

---
+++ Profiler/Profiler.h
@@ -0,0 +1,31 @@
+// version 1.0.0
+#pragma once
+
+#include <winnt.h>
+
+#define MAX_PROFILING_DATA_COUNT 50
+#define MAX_TAG_LENGTH 32
+
+/*************************** HOT TO USE ***************************/ 
+// ProfileBegin(L"test");
+// ...
+// ProfileEnd(L"test");
+// ProfileDataOutText(L"profile_result.txt");
+/******************************************************************/
+
+void ProfileBegin(const WCHAR* tag); // call this function at the starting point
+void ProfileEnd(const WCHAR* tag);   // call this function at the end point
+void ProfileDataOutText(const WCHAR* fileName); // profiling data save
+void ProfileReset(void);
+
+#define PROFILE_ON
+
+#ifdef PROFILE_ON
+#define PROFILE_BEGIN(Tag) ProfileBegin(Tag)
+#define PROFILE_END(Tag) ProfileEnd(Tag)
+
+#else
+#define PROFILE_BEGIN(Tag)
+#define PROFILE_END(Tag) 
+
+#endif
---
+++ Profiler/Test.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <Windows.h>
+
+#include "Profiler.h"
+
+long long GetFibonacciRecursive(int n)
+{
+	if (n == 0)
+	{
+		return 0;
+	}
+
+	if (n == 1)
+	{
+		return 1;
+	}
+	return GetFibonacciRecursive(n - 2) + GetFibonacciRecursive(n - 1);
+}
+
+int main(void)
+{
+	for (int i = 0; i < 30; ++i)
+	{
+		PROFILE_BEGIN(L"fibonacci");
+		long long ret = GetFibonacciRecursive(i);
+		PROFILE_END(L"fibonacci");
+
+		printf("%lld\n", ret);
+	}
+
+	for (int i = 0; i < 30; ++i)
+	{
+		PROFILE_BEGIN(L"Sleep100");
+		Sleep(100);
+		PROFILE_END(L"Sleep100");
+	}
+
+	for (int i = 0; i < 100; ++i)
+	{
+		PROFILE_BEGIN(L"HelloWorld");
+		printf("Hello World!");
+		PROFILE_END(L"HelloWorld");
+	}
+
+	ProfileDataOutText(L"profile_test.txt");
+}
